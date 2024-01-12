650517271cf303fd07c6b8ef760114ccc30c879f
hygkui <hygkui@gmail.com>
2024-01-12 09:21:03 +0800

    坐标转换

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,17 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use std::path::PathBuf;
+
+use tauri_plugin_log::{Target, TargetKind};
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_log::Builder::default().targets([
+            Target::new(TargetKind::Webview),
+            Target::new(TargetKind::Folder { path: PathBuf::from(""), file_name: Some("log12".into()) }),
+        ]).build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import App from "./App.vue";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import { trace, info, error } from "@tauri-apps/plugin-log";
+
+// with LogTarget::Webview enabled this function will print logs to the browser console
+// const detach = await attachConsole();
+
+trace("Trace");
+info("Info");
+error("Error");
+
+// detach the browser console from the log stream
+// detach();
 
 createApp(App)
 .use(ElementPlus)
